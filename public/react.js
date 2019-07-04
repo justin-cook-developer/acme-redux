5d2c089db0610c5004b3abe74e742047b3cf2af2
Justin Cook <justin.cook.dev@gmail.com>
2019-07-04 08:41:24 -0700

    Refactored React components to use Redux.

--- public/react.js
+++ public/react.js
@@ -1,42 +1,107 @@
-const { HashRouter, Route, Link } = ReactRouterDOM;
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable react/react-in-jsx-scope */
+const { HashRouter, Route, Link, withRouter } = ReactRouterDOM;
 
 const Home = () => <h2>Home</h2>;
 
-const Nav = ({ pathname, products }) => {
-  const links = [
-    { text: 'Home', to: '/' },
-    { text: `Products (${products.length})`, to: '/products' },
-    { text: 'Create A Product', to: '/products/create' },
-  ];
-  return (
-    <div id="nav">
-      {links.map((link, idx) => (
-        <Link
-          key={idx}
-          to={link.to}
-          className={pathname === link.to ? 'active' : ''}
-        >
-          {link.text}
-        </Link>
-      ))}
-    </div>
-  );
-};
-const Products = ({ products, destroy }) => {
-  return (
-    <div>
-      <h2>Products</h2>
-      <ul id="products">
-        {products.map(product => (
-          <li key={product.id}>
-            <div>{product.name}</div>
-            <button onClick={() => destroy(product)}>Destroy</button>
-          </li>
+class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: store.getState().products,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.setState({ products: store.getState().products })
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const { products } = this.state;
+    const { pathname } = this.props.location;
+
+    const links = [
+      { text: 'Home', to: '/' },
+      { text: `Products (${products.length})`, to: '/products' },
+      { text: 'Create A Product', to: '/products/create' },
+    ];
+
+    return (
+      <div id="nav">
+        {links.map((link, idx) => (
+          <Link
+            key={idx}
+            to={link.to}
+            className={pathname === link.to ? 'active' : ''}
+          >
+            {link.text}
+          </Link>
         ))}
-      </ul>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
+
+const ConnectedNav = withRouter(Nav);
+
+class Products extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: store.getState().products,
+    };
+    this.destroy = this.destroy.bind(this);
+  }
+
+  async componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.setState({ products: store.getState().products })
+    );
+
+    try {
+      const { data } = await axios.get('/api/products');
+      store.dispatch(addProds(data));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  async destroy(product) {
+    try {
+      await axios.delete(`/api/products/${product.id}`);
+      store.dispatch(deleteProd(product));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    const products = this.state.products;
+    return (
+      <div>
+        <h2>Products</h2>
+        <ul id="products">
+          {products.map(product => (
+            <li key={product.id}>
+              <div>{product.name}</div>
+              <button onClick={() => this.destroy(product)}>Destroy</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 class ProductCreate extends React.Component {
   constructor() {
@@ -46,14 +111,28 @@ class ProductCreate extends React.Component {
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.create = this.create.bind(this);
   }
+
   onChange(ev) {
     this.setState({ [ev.target.name]: ev.target.value });
   }
+
   onSubmit(ev) {
     ev.preventDefault();
-    this.props.create(this.state, this.props.history);
+    this.create();
   }
+
+  async create() {
+    try {
+      const { data } = await axios.post('/api/products', this.state);
+      store.dispatch(addProd(data));
+      this.props.history.push('/products');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     const { name } = this.state;
     const { onSubmit, onChange } = this;
@@ -74,62 +153,18 @@ class ProductCreate extends React.Component {
   }
 }
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [],
-    };
-    this.destroy = this.destroy.bind(this);
-    this.create = this.create.bind(this);
-  }
-  async create(product, history) {
-    const response = await axios.post('/api/products', product);
-    const products = [...this.state.products, response.data];
-    this.setState({ products });
-    history.push('/products');
-  }
-  async destroy(product) {
-    await axios.delete(`/api/products/${product.id}`);
-    const products = this.state.products.filter(
-      _product => _product.id !== product.id
-    );
-    this.setState({ products });
-  }
-  async componentDidMount() {
-    const response = await axios.get('/api/products');
-    this.setState({ products: response.data });
-  }
-  render() {
-    const { products } = this.state;
-    const { destroy, create } = this;
-    return (
-      <HashRouter>
-        <h1>Acme Products</h1>
-        <Route
-          render={({ location }) => (
-            <Nav pathname={location.pathname} products={products} />
-          )}
-        />
-        <div id="content">
-          <Route exact path="/" component={Home} />
-          <Route
-            path="/products"
-            exact
-            render={() => (
-              <Products destroy={destroy} products={products} />
-            )}
-          />
-          <Route
-            path="/products/create"
-            render={({ history }) => (
-              <ProductCreate create={create} history={history} />
-            )}
-          />
-        </div>
-      </HashRouter>
-    );
-  }
-}
+const ConnectedForm = withRouter(ProductCreate);
+
+const App = () => (
+  <HashRouter>
+    <h1>Acme Products</h1>
+    <ConnectedNav />
+    <div id="content">
+      <Route exact path="/" component={Home} />
+      <Route path="/products" exact component={Products} />
+      <Route path="/products/create" component={ConnectedForm} />
+    </div>
+  </HashRouter>
+);
 const root = document.querySelector('#root');
 ReactDOM.render(<App />, root);
